e9faa8868b7fef388ce8b2d27e179066c6873649
Fayham Alom <alomfn@gmail.com>
2025-03-27 17:02:59 +0000

    added zod weather schema and function

---
+++ app/actions.ts
@@ -0,0 +1,43 @@
+'use server'
+
+import { WeatherData } from "@/types/weather";
+import { z } from "zod";
+
+const weatherSchema = z.object({
+    name: z.string(),
+    main: z.object({
+        temp: z.number(),
+        feels_like: z.number(),
+        humidity: z.number()
+    }),
+    weather: z.array(z.object({
+        main: z.string(),
+        description: z.string(),
+        icon: z.string(),
+    })),
+    wind: z.object({
+        speed: z.number(),
+    }),
+});
+
+export async function getWeatherData(city: string): Promise<{ data?: WeatherData, error?: string }> {
+    try{
+        if(!city.trim()){
+            return { error: "City name is required" }
+        }
+        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPENWEATHER_API_KEY}&units=metric`)
+        if (!res.ok) {
+            throw new Error("City not found")
+        }
+        const rawData = await res.json()
+        const data = weatherSchema.parse(rawData);
+        return { data }
+    } catch(error) {
+        if(error instanceof z.ZodError) {
+            return { error: "Invalid weather data was received" }
+        }
+        return {
+            error: error instanceof Error ? error.message : "Failed to fetch weather data"
+        }
+    }
+}
